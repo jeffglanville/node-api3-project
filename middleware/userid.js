8410b1b0eb350e4464d1ac5c98c449165bce8e53
Jeff Glanville <jeff.glanville@outlook.com>
2020-08-11 16:59:10 -0500

    delete user.js folder, update userid folder

---
+++ middleware/userid.js
@@ -0,0 +1,38 @@
+const users = require("../users/userDB")
+
+function validateUserId() {
+    return (req, res, next) => {
+        users.getById(req.params.id)
+        .then((user) => {
+            if(user) {
+                req.user = user
+                next()
+            }else {
+                res.status(400).json({
+                    message: "invalid user id"
+                })
+            }
+        })
+        .catch(next)
+    }
+}
+
+function validateUser() {
+    return (req, res, next) => {
+        user.insert(req.body.name, req.body)
+        .then((user) => {
+            if (!req.body) {
+                res.status(400).json({
+                    message: "missing user data"
+                })
+            }else if (!res.body.name) {
+                res.status(400).json({
+                    message: "missing required name field"
+                })
+            }else {
+                res.json(user)
+            }
+        })
+        .catch(next)
+    }
+}
